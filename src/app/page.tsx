ebac422c6b1732d1f339610bdcd20072f3bfd87d
ParsaProg <parsashaabani3@gmail.com>
2025-08-21 13:59:59 +0330

    change the page file for 2 lang website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 "use client";
+import fa from "../langs/fa.json";
+import en from "../langs/en.json";
+
 import { useStore } from "../stores/userStore";
 export default function Home() {
-  const { font, setFont } = useStore();
+  const { lang, setLang } = useStore();
   return (
     <div>
-      <h1 className="text-red-600 text-2xl text-center mt-5">سلام</h1>
+      <h1 className="text-red-600 text-2xl text-center mt-5">
+        {lang === "fa" ? fa.title : en.title}
+      </h1>
       <button
         onClick={() => {
-          if (font === "font-v") {
-            setFont("font-p");
+          if (lang === "fa") {
+            setLang("en");
           } else {
-            setFont("font-v");
+            setLang("fa");
           }
         }}
         className="flex justify-center items-center bg-white shadow-xl shadow-[#0000003c] rounded-lg border-[1px] border-slate-200 p-5 cursor-pointer mx-auto"
       >
-        Change Font
+        {lang === "fa" ? fa.title : en.title}
       </button>
     </div>
   );
