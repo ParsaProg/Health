e0ad06c4f79df9e0565d806611863e1f7cb6ca54
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 14:19:56 +0330

    fix tailwind config for header setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,27 @@
-/** @type(import('tailwindcss').Config) */
+import type { Config } from "tailwindcss";
 
-module.exports = {
+export default {
   darkMode: "class",
   content: [
-    "./app/**/*.{js, ts, jsx, tsx}",
-    "./components/**/*.{js, ts, jsx, tsx}",
-    "./src/**/*.{ts, tsx, js, jsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   theme: {
-    extends: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#B80023",
+          light: "#B80023",
+        },
+      },
+      fontFamily: {
+        v: ["Vazir", "sans-serif"], // Persian font
+        p: ["Poppins", "sans-serif"], // English font
+      },
+      
+    },
   },
   plugins: [],
-};
+} satisfies Config;
