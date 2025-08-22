1f7107bc3875fe1c945dc8a232a355c3023daf2d
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 16:16:27 +0330

    fix meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,15 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Health Platform",
     description: "Get your body healthy with this app",
-    images: ""
+    images:
+      "https://github.com/ParsaProg/Health/blob/main/public/images/c67cc114fc289a3eacc442da873053b2.jpg?raw=true",
   },
   twitter: {
+    card: "summary_large_image",
     title: "Health Platform",
     description: "Get your body healthy with this app",
+    images:
+      "https://github.com/ParsaProg/Health/blob/main/public/images/c67cc114fc289a3eacc442da873053b2.jpg?raw=true",
   },
 };
 
