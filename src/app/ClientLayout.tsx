67534f775bf1bb29eb97d4f898ba740c1ce1d447
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 20:09:30 +0330

    client layout

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import "./globals.css";
 
 import { ThemeProvider } from "next-themes";
+import Header from "@/components/ui/header/Header";
 
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [themeLocal, setThemeLocal] = useState<string>("system");
+  useEffect(() => {
+    const storageTheme = window.localStorage.getItem("theme") ?? "system";
+    setThemeLocal(storageTheme);
+  });
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme={themeLocal!} enableSystem>
+      <Header />
       {children}
     </ThemeProvider>
   );
