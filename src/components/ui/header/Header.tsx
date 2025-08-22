cd5a8a7ab36bd66d49bcc4f5e4d02f01b905a8ae
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 15:25:32 +0330

    fix the fonts and add font assets

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
               <div className="absolute top-1 left-6 w-4 h-4 bg-red-600 rounded-full animate-ping"></div>
               <div className="flex flex-col items-start">
                 <h1 className="font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-[#B80023] to-[#9600b8]">Health</h1>
-                <h1 className="font-[400] text-slate-600 text-sm">
+                <h1 className="font-light text-slate-600 text-sm">
                   Next-Gen Wellness
                 </h1>
               </div>
