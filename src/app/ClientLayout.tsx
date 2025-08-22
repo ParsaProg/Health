fcd0ad520250d8f93f58750c47f43e8ff0c573cf
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 14:20:49 +0330

    add client layout

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -2,17 +2,14 @@
 
 import "./globals.css";
 
-import FontHOCComponent from "../HOC/fontSetHOC";
 import { ThemeProvider } from "next-themes";
 
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <FontHOCComponent>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-      </ThemeProvider>
-    </FontHOCComponent>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
   );
 }
