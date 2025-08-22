c3ac5484b7384c825f0b450798319eb76d5fef82
ParsaProg <parsashaabani3@gmail.com>
2025-08-22 20:10:25 +0330

    backdrop blur

---
+++ src/components/ui/header/mobile/BackdropBlur.tsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { BackdropMobileHeader } from "../../../../interfaces/headerMobileBackdrop";
+
+export default function BackdropFilter({
+  isShowDialog,
+  setIsShowDialog,
+  isShowThemeDialog,
+  setIsShowThemeDialog,
+}: BackdropMobileHeader) {
+  return (
+    <AnimatePresence mode="wait">
+      {isShowDialog || isShowThemeDialog ? (
+        <motion.div
+          onClick={() => {
+            setIsShowDialog(false);
+            setIsShowThemeDialog(false);
+          }}
+          key="backdrop"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          style={{ backdropFilter: "blur(10px)" }}
+          className={`${
+            isShowDialog && "hidden"
+          } [@media(max-width:1030px)]:flex top-[100px] inset-0 h-[100vh] w-full fixed right-0 backdrop-blur-[10px]`}
+        />
+      ) : null}
+    </AnimatePresence>
+  );
+}
