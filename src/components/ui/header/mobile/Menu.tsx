a11bc4c27d801eb674639f36788646986fd2a03e
ParsaProg <parsashaabani3@gmail.com>
2025-08-23 11:06:34 +0330

    chore: Mobile menu update

--- src/components/ui/header/mobile/Menu.tsx
+++ src/components/ui/header/mobile/Menu.tsx
@@ -2,6 +2,7 @@ import cn from "@/utils/cn";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
 import type { MobileMenu } from "@/interfaces/mobileMenu";
+import { Github } from "lucide-react";
 
 export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
   return (
@@ -15,7 +16,7 @@ export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
             y: -50,
             scale: 0.8,
           }}
-          className="z-[9999] bg-white border-slate-300 border-[1px] rounded-lg absolute right-[0] top-14 w-[200px] p-5 flex items-start flex-col gap-y-6 overflow-hidden"
+          className="z-[9999] bg-white border-slate-300 border-[1px] rounded-lg absolute right-[0] top-14 w-[200px] p-5 flex items-start flex-col gap-y-4 overflow-hidden"
         >
           {navItems.map((val: any, _i: any) => {
             return (
@@ -31,16 +32,16 @@ export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
                 transition={
                   val.special
                     ? {
-                        delay: _i * 0.1,
+                        delay: _i * 0.05,
                         duration: 0.2,
                       }
-                    : { delay: _i * 0.1 }
+                    : { delay: _i * 0.05 }
                 }
                 key={_i}
                 className={
                   val.special === true
                     ? "w-full cursor-pointer flex items-center gap-x-2 rounded-lg rtl:bg-gradient-to-r ltr:bg-gradient-to-l from-[#B80023] to-[#b800a3] text-white p-3"
-                    : "group cursor-pointer flex items-center gap-x-1 w-full"
+                    : "group cursor-pointer flex items-center gap-x-1 w-full mb-2"
                 }
               >
                 {val.icon}
@@ -56,6 +57,11 @@ export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
               </motion.div>
             );
           })}
+          <hr className="border-slate-200 w-full"/>
+          <motion.a initial={{opacity: 0, x: -50}} animate={{opacity: 1, x: 0}} exit={{opacity: 0}} transition={{delay: 6 * 0.05}} className="font-[600] bg-gradient-to-r from-[#080808] to-[#080808df] p-3 rounded-lg w-full text-white flex items-cennter gap-x-2 justify-start" href="https://github.com/ParsaProg/Health" target="_blank">
+            <Github size={20} />
+            GitHub
+          </motion.a>
         </motion.div>
       )}
     </AnimatePresence>
