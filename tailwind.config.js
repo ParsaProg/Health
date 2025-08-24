3801dea871c4e17f13a4b7d9f56818b510b0202d
ParsaProg <parsashaabani3@gmail.com>
2025-08-24 16:20:12 +0330

    animations added tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,25 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#B80023",
+        },
+      },
+      keyframes: {
+        "gradient-x": {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
+      },
+      animation: {
+        "gradient-x": "gradient-x 10s ease infinite",
+      },
+      backgroundSize: {
+        "200": "200% 200%", // lets us use bg-[length:200%_200%]
+      },
+    },
   },
   plugins: [],
 };
