22b85cbc5205eab86953778ac9c4542b56cc5993
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 09:30:16 +0330

    container of video library created

--- src/components/ui/Container/VideoLibraryContainers.tsx
+++ src/components/ui/Container/VideoLibraryContainers.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import type { VideoLibrary } from "@/interfaces/videoLibrary";
 import { Eye, Play, Star, Target } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function VideoLibraryContainer() {
+export default function VideoLibraryContainer({
+  category,
+  level,
+  title,
+  description,
+  minuteTime,
+  starCount,
+  posterImage,
+}: VideoLibrary) {
   const [isLoad, setIsload] = useState<boolean>(false);
+
   return (
-    <div className="group hover:scale-[1.03] transition-all duration-500 cursor-pointer bg-white rounded-lg shadow-md shadow-[#0000001a] w-[500px] h-[500px] border-[1px] border-slate-300 flex flex-col items-start gap-y-3">
+    <div className="group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[520px] h-[500px] border-[1px] dark:border-slate-600 border-slate-300 flex flex-col items-start justify-center gap-y-3">
       <div className="relative w-full h-[280px] overflow-hidden rounded-t-lg">
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center text-black z-[999]">
           <div className="opacity-0 group-hover:opacity-[1] transition-all duration-500 text-white rounded-full p-5 bg-primary">
@@ -19,9 +29,7 @@ export default function VideoLibraryContainer() {
             onLoad={() => setIsload(true)}
             unoptimized
             alt="video_poster"
-            src={
-              "https://blog.weberknapp.com/hs-fs/hubfs/iStock-1255081458.jpg?width=460&height=307&name=iStock-1255081458.jpg"
-            }
+            src={posterImage}
             fill
             className={`${
               isLoad
@@ -32,33 +40,36 @@ export default function VideoLibraryContainer() {
         </div>
         <div className="absolute w-full top-3 flex px-3 items-center justify-between z-[20]">
           <div className="bg-primary text-white rounded-md p-2 text-sm">
-            Ergonomic
+            {category}
           </div>
           <div className="bg-white text-black rounded-md p-2 text-sm">
-            Advanced
+            {level}
           </div>
         </div>
       </div>
       <div className="m-5 flex flex-col items-start gap-5">
-        <section className="text-primary flex items-center gap-x-2 font-[400] text-md">
+        <section className="dark:text-red-400 text-primary flex items-center gap-x-2 font-[400] text-md">
           <Target />
-          Ergonomic
+          {category}
         </section>
         <div>
-          <h1 className="text-red-900 font-[500] text-2xl">
-            Posture Transformation System
-          </h1>
-          <p className="text-slate-500 font-[400] text-lg mt-1">
-            Fix years of damage with this proven correction method
+          <h1 className="dark:text-red-500 text-red-900 font-[500] text-2xl">{title}</h1>
+          <p className="dark:text-slate-400 text-slate-500 font-[400] text-lg mt-1">
+            {description}
           </p>
         </div>
         <div className="flex justify-between items-center w-full">
-          <div className="flex items-center gap-x-2 text-slate-500">
-            18 min
+          <div className="flex items-center gap-x-2 dark:text-slate-400 text-slate-500">
+            {minuteTime} min
             <div className="flex items-center gap-x-1">
               {[1, 2, 3, 4, 5].map((v, _i) => {
                 return (
-                  <Star key={_i} color="#E934BE" size={15} fill="#E934BE" />
+                  <Star
+                    key={_i}
+                    stroke="#E934BE"
+                    size={15}
+                    fill={v - Number(starCount) - 1 < 0 ? "#E934BE" : "white"}
+                  />
                 );
               })}
             </div>
@@ -66,7 +77,7 @@ export default function VideoLibraryContainer() {
 
           <button className="p-3 rounded-lg border-none bg-primary hover:bg-purple-800 text-white font-bold flex items-center gap-x-2 justify-center transition-all duation-500">
             Watch
-            <Eye size={20}/>
+            <Eye size={20} />
           </button>
         </div>
       </div>
