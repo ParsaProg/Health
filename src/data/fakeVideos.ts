1f5c364d3181a3c6c83cead9cc06a5bb659dc38e
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 08:56:43 +0330

    add fake videos data for map or forEach

---
+++ src/data/fakeVideos.ts
@@ -0,0 +1,29 @@
+export const FakeVideos = [
+  {
+    posterImage: "https://vasafitness.com/wp-content/uploads/2023/09/blog-images-Incorporating-Exercise-into-Your-Daily-Routine-2-1920x1037.jpg",
+    level: "Beginner",
+    category: "Exercise",
+    title: "Morning Energy Activation",
+    description: "Revolutionary 15-minute routine that transforms your entire day",
+    minuteTime: "15",
+    starCount: "4",
+  },
+  {
+    posterImage: "https://www.chhs.colostate.edu/krnc/wp-content/uploads/sites/22/2019/10/acvoctober-1000x750-c-right.jpg",
+    level: "Intermediate",
+    category: "Nutrition",
+    title: "Nutrition Science Decoded",
+    description: "Latest research on optimal nutrition for peak performance",
+    minuteTime: "22",
+    starCount: "5",
+  },
+  {
+    posterImage: "https://worldkings.org/Userfiles/Upload/images/LiberNovo%20Omni1.jpg",
+    level: "Advanced",
+    category: "Ergonomics",
+    title: "Posture Transformation System",
+    description: "Fix years of damage with this proven correction method",
+    minuteTime: "18",
+    starCount: "5",
+  },
+];
