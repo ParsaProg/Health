d79c316603c7a94bf916d9d136e67bd8f3b18763
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 09:44:08 +0330

    menu fixed

--- src/components/ui/header/mobile/Menu.tsx
+++ src/components/ui/header/mobile/Menu.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
 import type { MobileMenu } from "@/interfaces/mobileMenu";
 import { Github } from "lucide-react";
+import BackdropFilter from "./BackdropBlur";
 
 export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
   return (
@@ -16,7 +17,7 @@ export default function MobileMenu({ isShowDialog, navItems }: MobileMenu) {
             y: -50,
             scale: 0.8,
           }}
-          className="z-[9999] bg-white dark:bg-gray-950 border-slate-300 dark:border-slate-900 border-[1px] rounded-lg absolute right-[0] top-[60px] w-[270px] p-3 flex items-start gap-y-2 flex-col overflow-hidden"
+          className="z-[999999] bg-white dark:bg-gray-950 border-slate-300 dark:border-slate-900 border-[1px] rounded-lg absolute right-[0] top-[60px] w-[270px] p-3 flex items-start gap-y-2 flex-col overflow-hidden"
         >
           {navItems.map((val: any, _i: any) => {
             return (
