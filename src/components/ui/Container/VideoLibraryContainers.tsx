8401c146531b6d5dbe0e4d9a88c34821b71c555c
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 08:23:35 +0330

    video library container created start

--- src/components/ui/Container/VideoLibraryContainers.tsx
+++ src/components/ui/Container/VideoLibraryContainers.tsx
@@ -1,5 +1,26 @@
-export default function VideoLibraryContainer(){
-    return <div className="video-library">
-        
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function VideoLibraryContainer() {
+  const [isLoad, setIsload] = useState<boolean>(false);
+  return (
+    <div className="bg-white rounded-lg shadow-md shadow-[#0000001a] w-[500px] h-[500px] border-[1px] border-slate-300 flex flex-col items-start gap-y-3">
+      <div className="relative w-full h-[280px] overflow-hidden rounded-t-lg">
+        <div className="w-full h-[280px]">
+          <Image
+            onLoad={() => setIsload(true)}
+            unoptimized
+            alt="video_poster"
+            src={
+              "https://blog.weberknapp.com/hs-fs/hubfs/iStock-1255081458.jpg?width=460&height=307&name=iStock-1255081458.jpg"
+            }
+            fill
+            className={`${isLoad? "scale-[1] blur-0 grayscale-0": "grayscale scale-[0.9] blur-lg"} transition-all duration-1000`}
+          />
+        </div>
+      </div>
     </div>
-} 
+  );
+}
