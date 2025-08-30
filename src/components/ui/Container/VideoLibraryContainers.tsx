6001617a8f47c84e6428a189e8d6e29144ecad3d
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 09:43:32 +0330

    fix the blur backdrop in app

--- src/components/ui/Container/VideoLibraryContainers.tsx
+++ src/components/ui/Container/VideoLibraryContainers.tsx
@@ -17,14 +17,14 @@ export default function VideoLibraryContainer({
   const [isLoad, setIsload] = useState<boolean>(false);
 
   return (
-    <div className="group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[520px] h-[500px] border-[1px] dark:border-slate-600 border-slate-300 flex flex-col items-start justify-center gap-y-3">
+    <div className="z-10 group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[520px] h-[500px] border-[1px] dark:border-slate-600 border-slate-300 flex flex-col items-start justify-center gap-y-3">
       <div className="relative w-full h-[280px] overflow-hidden rounded-t-lg">
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center text-black z-[999]">
+        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center text-black">
           <div className="opacity-0 group-hover:opacity-[1] transition-all duration-500 text-white rounded-full p-5 bg-primary">
             <Play size={40} />
           </div>
         </div>
-        <div className="w-full h-[280px] z-[10] relative">
+        <div className="w-full h-[280px] relative">
           <Image
             onLoad={() => setIsload(true)}
             unoptimized
@@ -38,7 +38,7 @@ export default function VideoLibraryContainer({
             } transition-all duration-1000`}
           />
         </div>
-        <div className="absolute w-full top-3 flex px-3 items-center justify-between z-[20]">
+        <div className="absolute w-full top-3 flex px-3 items-center justify-between">
           <div className="bg-primary text-white rounded-md p-2 text-sm">
             {category}
           </div>
