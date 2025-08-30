35e3904b4f40a73daec40bb7fd0c6fe9ac756399
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 09:30:24 +0330

    video library interface | type created

---
+++ src/interfaces/videoLibrary.ts
@@ -0,0 +1,9 @@
+export interface VideoLibrary{
+    posterImage: string,
+    level: string,
+    category: string,
+    title: string,
+    description: string,
+    minuteTime: string,
+    starCount: string,
+} 
