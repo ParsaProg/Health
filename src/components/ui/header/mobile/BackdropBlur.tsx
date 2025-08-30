ebcc5e2f7ed45b1901c68d1a36bef22aa4e8150f
ParsaProg <parsashaabani3@gmail.com>
2025-08-30 09:43:58 +0330

    fix the main backdrop filter component

--- src/components/ui/header/mobile/BackdropBlur.tsx
+++ src/components/ui/header/mobile/BackdropBlur.tsx
@@ -22,7 +22,7 @@ export default function BackdropFilter({
           transition={{ duration: 0.5 }}
           className={`${
             isShowDialog && "hidden"
-          } [@media(max-width:1056)]:flex top-[80px] inset-0 h-[100vh] w-full fixed right-0 transition-all duration-200 ${
+          } [@media(max-width:1056)]:flex top-[80px] inset-0 h-[100vh] z-[9999] w-full fixed right-0 transition-all duration-200 ${
             isShowDialog && "backdrop-blur-[5px] dark:backdrop-blur-[10px]"
           }`}
         />
