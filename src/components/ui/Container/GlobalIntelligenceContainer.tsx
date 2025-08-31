d435a9d8c41ee4d9c3cb9fc65471035ad3e9d84c
ParsaProg <parsashaabani3@gmail.com>
2025-08-31 15:06:22 +0330

    global intelligence container responsived

--- src/components/ui/Container/GlobalIntelligenceContainer.tsx
+++ src/components/ui/Container/GlobalIntelligenceContainer.tsx
@@ -11,13 +11,13 @@ export default function GlobalIntellignece({
   description,
   time,
   title,
-  trend
+  trend,
 }: GlobalIntelligenceInterFace) {
   const [isLoad, setIsload] = useState<boolean>(false);
   return (
-    <div className="group z-10 group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[650px] h-[350px] border-[1px] dark:border-slate-800 border-slate-300 flex flex-row items-center justify-start">
-      <div className="left-side relative w-[280px] h-full overflow-hidden rounded-l-lg">
-        <div className="w-[350px] h-full relative">
+    <div className="overflow-hidden group z-10 group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] [@media(max-width:650px)]:flex-col [@media(min-width:650px)]:w-[650px] w-[100%] [@media(min-width:650px)]:h-[350px] h-auto border-[1px] dark:border-slate-800 border-slate-300 flex flex-row items-center justify-start">
+      <div className=" [@media(max-width:650px)]:w-full [@media(max-width:650px)]:h-[80vw] relative w-[400px] h-full overflow-hidden rounded-l-lg">
+        <div className="[@media(max-width:650px)]:w-full [@media(max-width:650px)]:h-[80vw] w-[400px] h-full relative">
           <Image
             onLoad={() => setIsload(true)}
             unoptimized
@@ -31,20 +31,22 @@ export default function GlobalIntellignece({
             } transition-all duration-1000`}
           />
         </div>
-        <div className="absolute w-full top-3 flex px-3 items-center justify-between">
-          <div className="bg-pink-500 animate-pulse flex items-center gap-x-2 text-white rounded-md p-2 text-sm">
-            <TrendingUp size={15} />
-            {"Trending"}
+        {trend && (
+          <div className="absolute w-full top-3 flex px-3 items-center justify-between">
+            <div className="bg-pink-500 animate-pulse flex items-center gap-x-2 text-white rounded-md p-2 text-sm">
+              <TrendingUp size={15} />
+              {"Trending"}
+            </div>
           </div>
-        </div>
+        )}
       </div>
-      <section className="flex flex-col gap-y-5 items-start w-full px-5">
+      <section className="flex flex-col gap-y-5 items-start w-full px-5 [@media(max-width:650px)]:py-5">
         <div className="flex flex-row items-center justify-between w-full">
           <div className="flex items-center gap-x-2 text-black dark:text-white">
             <Apple size={20} className="dark:text-pink text-pink-600" />
 
             <h5 className="px-2 py-1 rounded-lg border-[1px] border-slate-400 dark:border-slate-400 text-slate-700 dark:text-slate-300 text-sm">
-              Nutrition
+              {category}
             </h5>
           </div>
           <div className="font-[400] text-slate-700 dark:text-slate-400 flex items-center gap-x-1">
@@ -53,12 +55,10 @@ export default function GlobalIntellignece({
         </div>
         <div className="flex flex-col gap-y-2">
           <h1 className="dark:text-white text-pink-800 text-2xl font-[700] w-full">
-            Mediterranean Diet 3.0: The Evolution
+            {title}
           </h1>
           <p className="w-full leading-relaxed dark:text-slate-400 text-slate-500 font-[400]">
-            New meta-analysis reveals enhanced Mediterranean diet variations
-            that reduce cardiovascular disease risk by 67% and boost longevity
-            markers.
+            {description}
           </p>
         </div>
         <button className="hover:bg-pink-500 hover:text-white transition-all duration-500 p-3 rounded-lg text-md text-pink-500 border-none outline-none font-[500] flex items-center justify-center gap-x-2">
