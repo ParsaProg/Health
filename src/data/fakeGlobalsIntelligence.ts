b43d38e678acbe3f6dd9c85eb2192174828605af
ParsaProg <parsashaabani3@gmail.com>
2025-08-31 14:50:53 +0330

    add global intelligence interface | type

---
+++ src/data/fakeGlobalsIntelligence.ts
@@ -0,0 +1,40 @@
+import type { GlobalIntelligenceInterFace } from "@/interfaces/global-intelligence";
+
+export const FakeGlobalIntelligenceData: GlobalIntelligenceInterFace[] = [
+  {
+    posterImage:
+      "https://dailymedia.case.edu/wp-content/uploads/2016/08/19155153/woman-sleeping.jpg",
+    title: "Revolutionary Sleep Optimization Protocol",
+    category: "Research",
+    time: "2 hourse ago",
+    description:
+      "Stanford researchers unveil groundbreaking sleep enhancement techniques that improve cognitive function by 340% and extend lifespan. ",
+  },
+  {
+    posterImage:
+      "https://cdn.mos.cms.futurecdn.net/C2pjtmUhJK5zyUhhBeKfnm-1200-80.jpg",
+    title: "Mediterranean Diet 3.0: The Evolution",
+    category: "Nutrition",
+    time: "6 hourse ago",
+    description:
+      "New meta-analysis reveals enhanced Mediterranean diet variations that reduce cardiovascular disease risk by 67% and boost longevity markers.",
+  },
+  {
+    posterImage:
+      "https://alleninstitute.org/wp-content/uploads/2024/05/Quantumconsciousness2-ezgif.com-crop.jpg",
+    title: "Mind-Body Connection: Quantum Breakthrough",
+    category: "Mental Health",
+    time: "1 days ago",
+    description:
+      "Neuroscientists discover how mental wellness directly rewrites genetic expression, offering new pathways to physical health optimization.",
+  },
+  {
+    posterImage:
+      "https://cdn.ainvest.com/aigc/hxcmp/images/compress-aime_generated_1755479805259.jpg.png",
+    title: "Ergonomic Revolution: AI-Powered Workspaces",
+    category: "Ergonomics",
+    time: "2 days ago",
+    description:
+      "Smart workspace technology reduces workplace injuries by 89% while increasing productivity through personalized ergonomic optimization.",
+  },
+];
