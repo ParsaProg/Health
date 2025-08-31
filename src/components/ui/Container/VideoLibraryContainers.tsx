be4b7863efe0d21f5c382959b46c24ca51f46876
ParsaProg <parsashaabani3@gmail.com>
2025-08-31 14:38:48 +0330

    main video library container scale animation create and fixed for 2 themes

--- src/components/ui/Container/VideoLibraryContainers.tsx
+++ src/components/ui/Container/VideoLibraryContainers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { VideoLibrary } from "@/interfaces/videoLibrary";
-import { Eye, Play, Star, Target } from "lucide-react";
+import { Apple, Eye, Play, Star, Sun, Target } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -17,7 +17,7 @@ export default function VideoLibraryContainer({
   const [isLoad, setIsload] = useState<boolean>(false);
 
   return (
-    <div className="z-10 group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[520px] h-[500px] border-[1px] dark:border-slate-600 border-slate-300 flex flex-col items-start justify-center gap-y-3">
+    <div className="z-10 group hover:scale-[1.03] transition-all duration-500 cursor-pointer dark:bg-slate-900 bg-white rounded-lg shadow-md dark:shadow-[#ffffff15] shadow-[#0000001a] w-[520px] h-[500px] border-[1px] dark:border-slate-800 border-slate-300 flex flex-col items-start justify-center gap-y-3">
       <div className="relative w-full h-[280px] overflow-hidden rounded-t-lg">
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center text-black">
           <div className="opacity-0 group-hover:opacity-[1] transition-all duration-500 text-white rounded-full p-5 bg-primary">
@@ -48,12 +48,20 @@ export default function VideoLibraryContainer({
         </div>
       </div>
       <div className="m-5 flex flex-col items-start gap-5">
-        <section className="dark:text-red-400 text-primary flex items-center gap-x-2 font-[400] text-md">
-          <Target />
+        <section className="dark:text-white text-primary flex items-center gap-x-2 font-[400] text-md">
+          {category === "Ergonomics" ? (
+            <Target />
+          ) : category === "Nutrition" ? (
+            <Apple />
+          ) : (
+            <Sun />
+          )}
           {category}
         </section>
         <div>
-          <h1 className="dark:text-red-100 text-red-900 font-[500] text-2xl">{title}</h1>
+          <h1 className="dark:text-red-100 text-red-900 font-[500] text-2xl">
+            {title}
+          </h1>
           <p className="dark:text-slate-400 text-slate-500 font-[400] text-lg mt-1">
             {description}
           </p>
