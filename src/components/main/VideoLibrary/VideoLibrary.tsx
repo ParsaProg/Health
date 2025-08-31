3af4c488e71a4b4aa1bd1b5515328469fde2a197
ParsaProg <parsashaabani3@gmail.com>
2025-08-31 14:38:05 +0330

    the main video library section created and changes for scale animation in android for responsive

--- src/components/main/VideoLibrary/VideoLibrary.tsx
+++ src/components/main/VideoLibrary/VideoLibrary.tsx
@@ -3,20 +3,21 @@
 import VideoLibraryContainer from "@/components/ui/Container/VideoLibraryContainers";
 import LandingSectionsStarter from "@/components/ui/landing/section-starter";
 import { FakeVideos } from "@/data/fakeVideos";
-import { motion } from "framer-motion";
 import { Play } from "lucide-react";
 
 export default function VideoLibrary() {
   return (
     <div className="w-[100%] flex flex-col items-center mt-24 gap-y-5 overflow-hidden">
       <LandingSectionsStarter
+        icon={<Play size={20} className="animate-rotate" />}
+        iconTitle="Video Content"
         title=" Health & Body"
         subTitle="Video Library"
         desc="Expert-curated video content featuring world-renowned health
         professionals and cutting-edge wellness techniques.Expert-curated video content featuring world-renowned health
         professionals and cutting-edge wellness techniques."
       />
-      <div className="flex items-center justify-center gap-10 px-3 xl:px-[100px] xl:flex-nowrap flex-wrap w-full mt-5">
+      <div className="scale-[0.85] sm:scale-[1] flex items-center justify-center gap-10 px-3 xl:px-[100px] xl:flex-nowrap flex-wrap w-full">
         {FakeVideos.map((video, index) => {
           return <VideoLibraryContainer key={index} {...video} />;
         })}
