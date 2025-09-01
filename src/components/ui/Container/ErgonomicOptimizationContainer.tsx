a83929bc8c44b089f31adadaf9d7b651124cf121
ParsaProg <parsashaabani3@gmail.com>
2025-09-01 14:55:36 +0330

    fix the ergonomic tips container ui section,

--- src/components/ui/Container/ErgonomicOptimizationContainer.tsx
+++ src/components/ui/Container/ErgonomicOptimizationContainer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { Eye, Star } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
@@ -36,13 +37,13 @@ export default function ErgonomicOptimizationContainer() {
       </div>
       <section className="mx-5 flex flex-col gap-y-5 justify-between h-full">
         <section className="flex flex-col gap-y-8">
-          <div className="transition-all duration-200 flex flex-row items-center gap-x-2 font-[600] text-[23px] group-hover:text-pink-600 text-pink-900">
-            <div className="group-hover:scale-[1.05] transition-all duration-200 p-3 rounded-xl bg-pink-200 text-pink-800">
+          <div className="transition-all duration-200 flex flex-row items-center gap-x-2 font-[600] text-[23px] group-hover:text-pink-600 text-pink-900 dark:text-white">
+            <div className="group-hover:scale-[1.05] transition-all duration-200 p-3 rounded-xl bg-pink-200 dark:bg-slate-200 dark:text-black text-pink-800">
               <Eye size={25} />
             </div>
             Quantum Desk Optimization
           </div>
-          <p className="text-slate-500 dark:text-slate-500 font-[400] text-lg">
+          <p className="text-slate-500 dark:text-slate-400 font-[400] text-lg">
             AI-powered workspace configuration that adapts to your unique
             biomechanics in real-time
           </p>
@@ -52,9 +53,9 @@ export default function ErgonomicOptimizationContainer() {
                 return (
                   <div
                     key={index}
-                    className="flex flex-row items-center gap-x-2 text-pink-700 font-[500] text-sm"
+                    className="flex flex-row items-center gap-x-2 text-pink-700 dark:text-pink-300 font-[500] text-sm"
                   >
-                    <div className="rounded-full bg-pink-600 w-3 h-3"></div>
+                    <div className="rounded-full bg-pink-600 dark:bg-pink-500 w-3 h-3"></div>
                     <h5>{tile}</h5>
                   </div>
                 );
@@ -62,10 +63,15 @@ export default function ErgonomicOptimizationContainer() {
             )}
           </section>
         </section>
-        <button className="flex items-center justify-center gap-x-2 border-none outline-none bg-gradient-to-r from-pink-600 to-red-500 w-full text-white rounded-lg py-2">
+        <motion.button
+          whileTap={{
+            scale: 0.95,
+          }}
+          className="flex items-center justify-center gap-x-2 border-none outline-none bg-gradient-to-r from-pink-600 to-red-500 w-full text-white rounded-lg py-2"
+        >
           <Star size={18} />
           Start Protocol
-        </button>
+        </motion.button>
       </section>
     </div>
   );
