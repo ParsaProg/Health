ecce57a10b1f2ce0beee7a0e427475b4aa94bb8b
ParsaProg <parsashaabani3@gmail.com>
2025-09-01 15:03:38 +0330

    create model interface of ergonomic section

---
+++ src/interfaces/ergonomicTipsSection.ts
@@ -0,0 +1,9 @@
+export default interface ErgonomicTipsInterFace{
+    posterImage: string;
+    level: string;
+    titleTime: string;
+    category: string;
+    title: string;
+    description: string;
+    subList: Array<string>;
+}
