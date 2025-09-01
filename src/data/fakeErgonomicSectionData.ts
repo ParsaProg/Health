d50220943418ff9c310c11c83e54db3e20b31d51
ParsaProg <parsashaabani3@gmail.com>
2025-09-01 15:03:28 +0330

    create fake model data

---
+++ src/data/fakeErgonomicSectionData.ts
@@ -0,0 +1,43 @@
+import type ErgonomicTipsInterFace from "@/interfaces/ergonomicTipsSection";
+
+export const FakeErgonomicData: ErgonomicTipsInterFace[] = [
+    {
+        posterImage: "https://businesscasestudies.co.uk/wp-content/uploads/2025/03/image-1994.jpg",
+        title: "Quantum Desk Optimization",
+        description: "AI-powered workspace configuration that adapts to your unique biomechanics in real-time",
+        level: "Advance",
+        titleTime: "30 min setup",
+        category: "eye",
+        subList: [
+            "Neural monitor positioning",
+            "Biometric chair adjustment",
+            "Circadian lighting sync"
+        ]
+    },
+    {
+        posterImage: "",
+        title: "Quantum Desk Optimization",
+        description: "AI-powered workspace configuration that adapts to your unique biomechanics in real-time",
+        level: "Advance",
+        titleTime: "30 min setup",
+        category: "eye",
+        subList: [
+            "Neural monitor positioning",
+            "Biometric chair adjustment",
+            "Circadian lighting sync"
+        ]
+    },
+    {
+        posterImage: "",
+        title: "Quantum Desk Optimization",
+        description: "AI-powered workspace configuration that adapts to your unique biomechanics in real-time",
+        level: "Advance",
+        titleTime: "30 min setup",
+        category: "eye",
+        subList: [
+            "Neural monitor positioning",
+            "Biometric chair adjustment",
+            "Circadian lighting sync"
+        ]
+    },
+]
